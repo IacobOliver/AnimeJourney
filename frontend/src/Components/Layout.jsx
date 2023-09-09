0864a0268645249a6a89e346a0c7baf5f951c03e
IacobOliver <olialexander08@gmail.com>
2023-09-09 13:12:55 +0300

    setup the router

---
+++ frontend/src/Components/Layout.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Layout(){
+    return <div className="bg-blue-300 w-5 h-5 ">Layout</div>
+}
---
+++ frontend/src/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./index.css"
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Explore  from "./Pages/Explore"
+import Layout from "./Components/Layout";
+
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Layout/>
+      <Routes>
+        <Route path="/" element={<Layout />}> </Route>
+        <Route path="/explore" element={<Explore />}> </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
