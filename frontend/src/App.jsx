74aba4ac376c079fe78ebb3d1c28265a027e9cd3
IacobOliver <olialexander08@gmail.com>
2023-09-09 11:23:18 +0300

    fixed tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 
 import './App.css'
+import React from 'react'
+import {useEffect, useState} from "react"
 
 function App() {
+
+
+  fetch("http://localhost:3000/popular")
+  .then((response) => response.json())
+  .then((animelist) => console.log(animelist));
+
  
   return (
     <>
-    
+    <div className="w-5 h-5 bg-blue-600"></div>
     </>
   )
 }
