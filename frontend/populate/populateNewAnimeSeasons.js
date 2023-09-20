713fd3a806f4542bc4dbb5a908148eb895c99cb8
Oliver <olialexander08@gmail.com>
2023-09-20 16:27:02 +0300

    populate New Anime done

--- frontend/populate/populateNewAnimeSeasons.js
+++ frontend/populate/populateNewAnimeSeasons.js
@@ -3,52 +3,45 @@
 let numberOfPages;
 let anime = []
 
-const populateTopAnime = () => {
+const extractDetailsFromPage = (animeList, pageNumber) =>{
+    console.log("----------------------------------------------------PAGE ",pageNumber)
+    animeList.map( item => anime.push({
+        animeId : item.mal_id,
+        name : item.title,
+        image : item.images.jpg.image_url,
+        airedFrom : item.aired.from.substring(0,10),
+        animeType : item.type
+    }))
+    
+}
 
-    const getTopAnimePages = (numberOfPages) => {
-        if (numberOfPages > 0) {
-            fetch(`https://api.jikan.moe/v4/top/anime?page=${numberOfPages}`)
+const populateNewSesonAnime = () => {
+    fetch(`https://api.jikan.moe/v4/seasons/now?page=${1}`)
+    .then(res => res.json())
+    .then(data => {
+        numberOfPages = data.pagination.last_visible_page;
+        extractDetailsFromPage(data.data, 1)
+        getNewSesonAnime(numberOfPages);
+    })
+    .catch(err => console.error(err))
+
+
+    const getNewSesonAnime = (numberOfPages) => {
+        if (numberOfPages > 1) {
+            fetch(`https://api.jikan.moe/v4/seasons/now?page=${numberOfPages}`)
                 .then(res => res.json())
                 .then(data => {
-                    console.log("NEW PAGE -------------------------------------------------------", numberOfPages)
-
-                    const getImagesForAnime = (animeObjectIndex) =>{
-                        if(animeObjectIndex >= 0){
-                            fetch(`https://api.jikan.moe/v4/anime/${data.data[animeObjectIndex].mal_id}/pictures`)
-                            .then(res => res.json())
-                            .then(animeImages =>{
-                                console.log(extractImages(animeImages), " from anime " , data.data[animeObjectIndex].title, " " ,data.data[animeObjectIndex].rank )
-                                anime.push({
-                                    animeId : data.data[animeObjectIndex].mal_id,
-                                    name : data.data[animeObjectIndex].title,
-                                    rating : data.data[animeObjectIndex].score,
-                                    numberOfReviews : data.data[animeObjectIndex].scored_by,
-                                    animeDescription : data.data[animeObjectIndex].synopsis,
-                                    images : extractImages(animeImages)
-                                })
-                            })
-                            .catch(err => comsole.error(err))
-
-                            setTimeout(() =>{
-                                getImagesForAnime(animeObjectIndex -1)
-                            },1000)
-
-                        }else{
-                            setTimeout(() => {
-                                getTopAnimePages(numberOfPages - 1)
-                            }, 1000)
-                        }
-                    }
-
-                    getImagesForAnime(data.data.length -1)
+                    extractDetailsFromPage(data.data, numberOfPages)
+                  
+                    setTimeout(() =>{
+                        getNewSesonAnime(numberOfPages -1)
+                    },1000)
 
                 })
-                .catch(err => console.error(err))
-
-           
+                .catch(err => console.error(err)) 
         } else {
             console.log("fetch")
-            fetch("http://localhost:8080/topAnime/addAnime", {
+            fetch("http://localhost:8080/newAnimeSeasons/addAnimes", {
                 method : "POST",
                 headers : {
                     "Content-Type" : "application/json"
@@ -61,11 +54,11 @@ const populateTopAnime = () => {
         }
     }
 
-    getTopAnimePages(numberOfPages);
+    
 
 }
 
 
 
 
-populateTopAnime()
+populateNewSesonAnime()
