0e92096aeea90be2a39af15ef42a959a90b6b6b2
IacobOliver <olialexander08@gmail.com>
2023-09-23 14:30:56 +0300

    small c hanges upcoming anime

--- frontend/src/Components/ExploreComponents/UpcomigAnime.jsx
+++ frontend/src/Components/ExploreComponents/UpcomigAnime.jsx
@@ -16,15 +16,18 @@ export default function UpcomingAnime() {
     }, [])
 
     const AnimeCard = ({anime}) => {
+       
         return (
-            <div className=" h-16 my-2 rounded-lg flex">
-                {/* <img className="h-full w-14 flex justify-center items-center rounded-lg" src={anime.images.jpg.image_url}/> */}
+            <div className=" bg-black_second_theme h-16 my-2 rounded-lg flex">
+                 <p className=" text-fifth_color_theme font-serif font-semibold text-left text-sm">{anime.type}</p>
+
                 <div className="h-full w-16 rounded-lg bg-center bg-cover p-2" style={{ backgroundImage: `url(${anime.images.jpg.image_url})` }}></div>
 
-                <div className="text-fifth_color_theme ml-3 font-fantasy w-72">
+                <div className="text-fifth_color_theme ml-3 font-fantasy w-72 flex flex-col ">
                 <p className=" text-fifth_color_theme font-serif font-semibold text-left text-md line-clamp-2 tracking-normal">{anime.title}</p>
-                <p className=" text-fifth_color_theme font-serif font-semibold text-left text-sm">{anime.type}</p>
                 </div>
+               
+                <p className=" text-gray-400 font-serif font-semibold text-left text-xs tracking-normal">{anime.aired.string.split("to")[0]}</p>
 
             </div>)
     }
