cbce93de915cd4c4749ba146c093698894104edf
IacobOliver <olialexander08@gmail.com>
2023-10-20 16:52:39 +0300

    hei

---
+++ backend/src/main/java/AnimeJourney/anime/controller/SavedUserAnimeDetailsController.java
@@ -0,0 +1,2 @@
+package AnimeJourney.anime.controller;public class SavedUserAnimeDetailsController {
+}
---
+++ backend/src/main/java/AnimeJourney/anime/service/SavedUserAnimeDetailsService.java
@@ -0,0 +1,2 @@
+package AnimeJourney.anime.service;public class SavedUserAnimeDetailsService {
+}
