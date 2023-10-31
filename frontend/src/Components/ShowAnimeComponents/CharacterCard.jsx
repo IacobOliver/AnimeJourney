5b31c6d5d6af7ec8b85bad16857a7a073031acbe
IacobOliver <olialexander08@gmail.com>
2023-10-31 15:07:12 +0200

    modifyed the character card to fit in more places and updated allVoiceActors

--- frontend/src/Components/ShowAnimeComponents/CharacterCard.jsx
+++ frontend/src/Components/ShowAnimeComponents/CharacterCard.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 
-export default function CharacterCard({character, index}){
+export default function CharacterCard({character, voiceActor , withCharacterDetails}){
     return (
-        <div key={index} href={character.character.url} className="w-full md:h-20 rounded-lg bg-[rgb(20,20,20)] flex flex-col md:flex-row md:justify-between relative font font-fantasy tracking-wide text-fifth_color_theme">
-        <a href={character.character.url} target="_blank" className="w-full h-full absolute" ></a>
+        <div href={character.character.url} className="w-full md:h-20 rounded-lg bg-[rgb(20,20,20)] flex flex-col md:flex-row md:justify-between relative font font-fantasy tracking-wide text-fifth_color_theme mb-1">
+        {/* <a href={character.character.url} target="_blank" className="w-full h-full absolute" ></a> */}
 
-        <div className="flex md:!bg-none bg-center bg-cover w-[9rem] h-[12rem] md:w-auto md:h-auto" style={{ backgroundImage: `url(${character.character.images.jpg.image_url})` }}>
+        { withCharacterDetails && <div className="flex md:!bg-none bg-center bg-cover w-[9rem] h-[12rem] md:w-auto md:h-auto" style={{ backgroundImage: `url(${character.character.images.jpg.image_url})` }}>
             <img className="hidden md:flex h-full w-14 rounded-l-lg" src={character.character.images.jpg.image_url} />
 
             <div className="flex flex-col justify-end md:justify-start md:ml-3 mt-1 w-full">
                 <p className="text-md truncate-paragraph xl:text-2xl w-full md:w-auto bg-[rgba(0,0,0,0.8)] md:bg-transparent pl-1 md:pl-0">{character.character.name}</p>
                 <p className="hidden md:flex md:text-sm xl:text-md">{character.role}</p>
             </div>
-        </div>
-
-        <div className="w-[9rem] h-[12rem] md:w-auto  md:h-full flex md:ml-2">
-            {character.voice_actors[0] ?
+        </div>}
+        {/* ${withCharacterDetails ? "justify-between" : ""} */}
+        <div className={`w-[9rem] h-[12rem]   md:h-full flex  md:ml-2 ${withCharacterDetails ? "md:w-auto" : "md:w-full"}`}>
+            { voiceActor ?
                 <>
-                    <div className="flex w-full items-start justify-end md:justify-start md:items-end flex-col md:mr-2 md:mt-1 bg-center bg-cover md:!bg-none" style={{ backgroundImage: `url(${character.voice_actors[0].person.images.jpg.image_url})` }}>
-                        <p className="w-full truncate-paragraph text-left md:text-right text-md xl:text-lg bg-[rgba(0,0,0,0.8)] md:bg-transparent pl-1 md:pl-0">{character.voice_actors[0].person.name}</p>
-                        <p className="hidden md:flex md:text-sm xl:text-md">{character.voice_actors[0].language}</p>
+                    <div className={`flex w-full items-start justify-end md:justify-start ${withCharacterDetails ? "md:items-end" : ""}  flex-col md:mr-2 md:mt-1 bg-center bg-cover md:!bg-none`} style={{ backgroundImage: `url(${voiceActor.person.images.jpg.image_url})` }}>
+                        <p className={`w-full truncate-paragraph text-left ${withCharacterDetails ? "md:text-right" : "md:text-left"}  text-md xl:text-lg bg-[rgba(0,0,0,0.8)] md:bg-transparent pl-1 md:pl-0`}>{voiceActor.person.name}</p>
+                        <p className="hidden md:flex md:text-sm xl:text-md">{voiceActor.language}</p>
                     </div>
 
-                    <img className="hidden md:flex md:h-full rounded-r-lg w-14" src={character.voice_actors[0].person.images.jpg.image_url} />
+                    <img className="hidden md:flex md:h-full rounded-r-lg w-14" src={voiceActor.person.images.jpg.image_url} />
                 </>
                 :
                 <div className="h-full w-full flex">
--- frontend/src/Pages/AllAnimeCharacters.jsx
+++ frontend/src/Pages/AllAnimeCharacters.jsx
@@ -4,6 +4,11 @@ import { useState } from "react";
 import { useParams } from "react-router-dom";
 import CharacterCard from "../Components/ShowAnimeComponents/CharacterCard";
 import Loading from "../Components/Loading";
+import {
+    Popover,
+    PopoverHandler,
+    PopoverContent,
+  } from "@material-tailwind/react";
 
 export default function AllAnimeCharacters(){
     const [characters, setCharacters] = useState(null);
@@ -19,15 +24,38 @@ export default function AllAnimeCharacters(){
     },[])
 
 
+    const AllVoiceActorsPopOver = ({character}) =>{
+        return(
+            <Popover>
+            <PopoverHandler>
+              <div> <CharacterCard character={character} index={0} voiceActor={character.voice_actors[0]} withCharacterDetails={true}/></div>
+            </PopoverHandler>
+            <PopoverContent className="z-[999] bg-[rgba(0,0,0,0.7)] backdrop-blur-md">
+              <div className="">
+                {character.voice_actors.length > 1 ? character.voice_actors.map((actor, index) =>{
+                    if(index > 0){
+                        return <CharacterCard character={character} index={index} voiceActor={actor}/>
+                    }
+                })
+                :
+                <p className=" text-fifth_color_theme text-lg font-fantasy tracking-wide ">No other voice actors ...</p>            
+                }
+              </div>
+             
+            </PopoverContent>
+          </Popover>
+        )
+    }
+
+
 
     return(
-        <div className="grid grid-cols-3 gap-3 p-3 mt-2">
+        <div className="grid grid-cols-3 gap-2 p-3 mt-2">
+
             {characters ? 
-            characters.map((character, index) => <CharacterCard character={character} index={index}/>)
+            characters.map((character, index) => <AllVoiceActorsPopOver key={index} character={character}/>) 
             :
             <Loading/>
-        
-        
         }
 
         </div>
